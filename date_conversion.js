45793d3dba678ea95d65c810a84edbf3d1237f86
Mark Ellis <markellisdev@gmail.com>
2017-04-27 11:41:12 -0500

    initial commit

---
+++ date_conversion.js
@@ -0,0 +1,12 @@
+// From here https://www.testdome.com/questions/javascript/date/8521?testId=14&testDifficulty=Easy
+
+// <------------------ INSTRUCTIONS ---------------------->
+// Write a function that converts user entered date formatted as M/D/YYYY to a format required by an API (YYYYMMDD). The parameter "userDate" and the return value are strings.
+// For example, it should convert user entered date "12/31/2014" to "20141231" suitable for the API.
+
+function formatDate(userDate) {
+  // format from M/D/YYYY to YYYYMMDD
+
+}
+
+console.log(formatDate("12/31/2014"));
