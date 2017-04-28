69b8e86ccdc5a6e52fe0fd94c728ba5dd3defcba
Mark Ellis <markellisdev@gmail.com>
2017-04-28 09:20:31 -0500

    code cleaned and commenting added

--- date_conversion.js
+++ date_conversion.js
@@ -3,15 +3,22 @@
 // <------------------ INSTRUCTIONS ---------------------->
 // Write a function that converts user entered date formatted as M/D/YYYY to a format required by an API (YYYYMMDD). The parameter "userDate" and the return value are strings.
 // For example, it should convert user entered date "12/31/2014" to "20141231" suitable for the API.
+//
+// Must pass the following test cases
+// Example case: Wrong answer
+// Two-digit month and day: Wrong answer
+// One-digit day: Wrong answer
+// One-digit month: Wrong answer
 
 function formatDate(userDate) {
     // format from M/D/YYYY to YYYYMMDD
+    // Solution - cut the month and day out, then add all back together
     var howLong = userDate.length;
     var year = userDate.slice (howLong - 4);
     var month = userDate.substring(0, userDate.indexOf("/"));
     var day = userDate.substring((userDate.indexOf("/") + 1), userDate.lastIndexOf("/"));
-    // cut the month and day out, then add all back together
-    console.log("Month length: ", month.length)
+
+    //If either month or day are shorter than two characters, convert by adding a leading 0
     if (month.length < 2) {
         month = 0 + month;
     }
