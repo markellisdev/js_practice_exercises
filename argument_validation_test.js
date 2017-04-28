5cfe8b9bfdd45502e5864543340b6aae00b71612
Mark Ellis <markellisdev@gmail.com>
2017-04-28 09:52:20 -0500

    problem solved, but not sure why it only works one way

---
+++ argument_validation_test.js
@@ -0,0 +1,20 @@
+/* Created by markellis on 4/28/17. */
+// Implement the ensure function so that it throws an error if called without arguments or an argument is undefined. Otherwise it should return the given value.
+
+// This was a simple exercise to validate input
+var error = "You did something wrong!"
+
+function ensure(value) {
+    if (value || typeof(value) !== 'undefined') {
+        return value;
+    }
+    //I'm not sure why the following won't work, but the above does
+    /* else if (typeof(value) === "undefined") {
+        throw(error);
+    } */
+    else {
+        throw(error);
+    }
+}
+
+ensure();
