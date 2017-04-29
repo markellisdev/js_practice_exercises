678b0e1a77a07916798a521713b4ee750f70458b
Mark Ellis <markellisdev@gmail.com>
2017-04-29 13:43:44 -0500

    initial instructions for exercise

---
+++ image_remove.js
@@ -0,0 +1,36 @@
+// https://www.testdome.com/questions/javascript/imagegallery/8527?testId=14&testDifficulty=Easy
+
+/* An image gallery is a set of images with corresponding remove buttons. This is the HTML code for a gallery with
+ two images:
+
+    <div class="image">
+        <img src="https://goo.gl/2oZU2S" alt="First">
+        <button class="remove">X</button>
+    </div>
+    <div class="image">
+        <img src="https://goo.gl/tniGAc" alt="Second">
+        <button class="remove">X</button>
+    </div>
+Implement the registerClickHandler function that registers a click event handler and implements the following logic: When the button of class remove is clicked, its parent <div> element should be removed from the gallery.
+
+For example, after the first image has been removed from the gallery above, it's HTML code should look like this:
+
+<div class="image">
+    <img src="https://goo.gl/tniGAc" alt="Second">
+    <button class="remove">X</button>
+</div> */
+
+function registerClickHandler () {
+    // Implement the click handler here for button of class 'remove'
+}
+
+/* HTML code for testing purposes (do not submit uncommented):
+ <div class="image">
+ <img src="https://goo.gl/2oZU2S" alt="First">
+ <button class="remove">X</button>
+ </div>
+ <div class="image">
+ <img src="https://goo.gl/tniGAc" alt="Second">
+ <button class="remove">X</button>
+ </div>
+ */
