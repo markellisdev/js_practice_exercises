0576c2f7f908bc5dd5b6aaa940f29391c24e2aed
Mark Ellis <markellisdev@gmail.com>
2017-05-08 11:19:10 -0500

    basic work with strings - refresher

--- dbanas_tut_pt1.js
+++ dbanas_tut_pt1.js
@@ -1,5 +1,15 @@
 /**
  * Created by markellis on 5/7/17.
  */
+// Minimum and Max Values
 document.write(Number.MAX_VALUE+"<br />");
-document.write(Number.MIN_VALUE+"<br />");
+document.write(Number.MIN_VALUE+"<br />" +"<br />");
+
+// Common escaped characters
+var samp_str = "Here are some escaped characters \" \' \\ \t \n";
+document.write(samp_str + "<br />" + "<br />");
+
+//Adding strings together
+var first_str = "First String ";
+var second_str = "Second String";
+document.write(first_str + second_str);
